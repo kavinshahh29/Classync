b23e8afe7dd838cd93850d206d393b7852334c70
Kris0011 <krishp759@gmail.com>
2025-03-09 18:25:02 +0530

    Refreshed .gitignore

--- server/src/main/java/com/classync/project/DTO/DoubtDto.java
+++ server/src/main/java/com/classync/project/DTO/DoubtDto.java
@@ -1,13 +1,56 @@
 package com.classync.project.DTO;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
+
+
 public class DoubtDto {
     private String title;
     private String content;
     private int userId;
     private Long classroomId;
+
+
+    public DoubtDto() {
+    }
+
+    public DoubtDto(String title, String content, int userId, Long classroomId) {
+        this.title = title;
+        this.content = content;
+        this.userId = userId;
+        this.classroomId = classroomId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+
+    public Long getClassroomId() {
+        return classroomId;
+    }
+
+
+
+
 }
--- server/src/main/java/com/classync/project/DTO/SolutionDto.java
+++ server/src/main/java/com/classync/project/DTO/SolutionDto.java
@@ -3,10 +3,45 @@ package com.classync.project.DTO;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+
 public class SolutionDto {
     private Long doubtId;
     private int userId;
     private String content;
+
+
+    public SolutionDto() {
+    }
+
+    public SolutionDto(Long doubtId, int userId, String content) {
+        this.doubtId = doubtId;
+        this.userId = userId;
+        this.content = content;
+    }
+
+    public Long getDoubtId() {
+        return doubtId;
+    }
+
+    public void setDoubtId(Long doubtId) {
+        this.doubtId = doubtId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
 }
